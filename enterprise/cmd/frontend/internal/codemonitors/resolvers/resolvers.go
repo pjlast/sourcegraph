3272ab73b54e0f7789e57bfe03bd4bd221f216a9
Camden Cheek <camden@ccheek.com>
2021-11-13 18:32:37 -0700

    fixup some build errors

--- enterprise/cmd/frontend/internal/codemonitors/resolvers/resolvers.go
+++ enterprise/cmd/frontend/internal/codemonitors/resolvers/resolvers.go
@@ -512,7 +512,7 @@ func (r *Resolver) actionConnectionResolverWithTriggerID(ctx context.Context, tr
 		return nil, err
 	}
 
-	totalCount, err := r.store.TotalCountActionEmails(ctx, monitorID)
+	totalCount, err := r.store.CountEmailActions(ctx, monitorID)
 	if err != nil {
 		return nil, err
 	}
