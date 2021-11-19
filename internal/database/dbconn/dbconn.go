2a67a5e86095b9b90eb79515d5fc6956c0620435
Eric Fritz <eric@eric-fritz.com>
2021-11-18 19:33:53 -0600

    Sequester globals.

--- internal/database/dbconn/dbconn.go
+++ internal/database/dbconn/dbconn.go
@@ -30,10 +30,6 @@ import (
 )
 
 var (
-	// Global is the global DB connection.
-	// Only use this after a call to SetupGlobalConnection.
-	Global *sql.DB
-
 	defaultDataSource      = env.Get("PGDATASOURCE", "", "Default dataSource to pass to Postgres. See https://pkg.go.dev/github.com/jackc/pgx for more information.")
 	defaultApplicationName = env.Get("PGAPPLICATIONNAME", "sourcegraph", "The value of application_name appended to dataSource")
 	// Ensure all time instances have their timezones set to UTC.
@@ -54,22 +50,6 @@ type Opts struct {
 	// AppName overrides the application_name in the DSN. This separate parameter is needed
 	// because we have multiple apps connecting to the same database, but have a single shared DSN configured.
 	AppName string
-}
-
-// SetupGlobalConnection connects to the given data source and stores the handle
-// globally.
-//
-// dbname is used for its Prometheus label value instead of whatever actual value is set in dataSource.
-// This is needed because in our dev environment we use a single physical database (and DSN) for all our different
-// logical databases. app, however is set as the application_name in the connection string. This is needed
-// because we have multiple apps connecting to the same database, but have a single shared DSN.
-//
-// Note: github.com/jackc/pgx parses the environment as well. This function will
-// also use the value of PGDATASOURCE if supplied and dataSource is the empty
-// string.
-func SetupGlobalConnection(opts Opts) (err error) {
-	Global, err = New(opts)
-	return err
 }
 
 // New connects to the given data source and returns the handle.
